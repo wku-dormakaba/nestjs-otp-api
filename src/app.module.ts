19be9fe805b58c7d8dc2b870170a9ab00666fcf9
William Ku <william.ku@dormakaba.com>
2023-11-05 11:31:44 +0800

    add event emitter and cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OtpModule } from './otp/otp.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [OtpModule],
+  imports: [OtpModule, EventEmitterModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
